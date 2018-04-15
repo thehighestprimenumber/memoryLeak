f035638b5adcd62629ad58e77b8c9d1af23cef21
nahueldl <deleonardisnahuel@gmail.com>
2018-04-15 15:59:39 -0300

    Agregadas estructuras de socket y funciones de cliente

---
+++ redistinto/src/sockets/cliente.c
@@ -0,0 +1,49 @@
+#include "socket.h"
+
+int connect_to_server(char * port){
+	struct addrinfo *server_info;
+	struct addrinfo hints;
+
+	memset(&hints, 0, sizeof(hints));//Preparamos el espacio para hints
+
+	hints.ai_family = AF_UNSPEC;    // Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
+	hints.ai_socktype = SOCK_STREAM;  // Indica que usaremos el protocolo TCP
+
+
+	getaddrinfo(IP, port, &hints, &server_info);  // Carga en server_info los datos de la conexion
+
+	//Si devuelve -1 tuvimos un problema
+	int server_socket = socket(server_info->ai_family, server_info->ai_socktype,server_info->ai_protocol);
+
+	// Si devuelve algo distinto de 0 tuvimos un problema
+	int retorno = connect(server_socket, server_info->ai_addr, server_info->ai_addrlen);
+
+	freeaddrinfo(server_info);//Liberamos addrinfo
+
+	//verificamos errores y devolvemos acorde
+	if(retorno != 0 || server_socket == -1){
+		return -1;
+	}else{
+		return server_socket;
+	}
+}
+
+int send_msg(int socket, Message msg) {
+	if(socket == -1 || msg.contenido == NULL || msg.header == NULL) return -1;
+
+	//Calculo el tamaño total
+	int sizeTotal = sizeof(ContentHeader) + msg.header->size;
+
+	//Creo y relleno un buffer
+	void * buffer = malloc(sizeTotal);
+    memcpy(buffer, msg.header, sizeof(ContentHeader));
+    memcpy(buffer + sizeof(ContentHeader), msg.contenido, msg.header->size);
+
+    //Envio y registro el resultado que es el tamaño de lo que devolvio o -1 en caso de error
+    int resultado = send(socket, buffer, sizeTotal, 0);
+
+    free(buffer);
+
+    if(resultado <= 0) return -1;
+    else return 1;
+}
---
+++ redistinto/src/sockets/socket.h
@@ -0,0 +1,42 @@
+
+#ifndef REDISTINTO_SOCKET_H_
+#define REDISTINTO_SOCKET_H_
+
+//Imports
+#include <sys/socket.h>
+
+
+
+//Defines
+#define IP "172.0.0.1"//todas estaran dentro de la maquina
+#define PUERTO_COORDINADOR "8001"
+#define PUERTO_PLANIFICADOR "8002"
+
+
+
+//Enums
+
+//Aca se iran agregando los id que identificaran a los mensajes con distinto objetivo
+typedef enum{IDEJMPLO1, IDEJMPLO2} tipoId;
+
+
+
+//Estructuras
+
+//Estructura del header
+typedef struct {
+	tipoId id;
+	int size;
+} __attribute__((packed)) ContentHeader;
+
+typedef struct {
+	ContentHeader *header;
+	void *contenido;
+} Message;
+
+//Funciones
+int  connect_to_server(char * port);
+int send_msg(int socket, Message msg);
+
+
+#endif
