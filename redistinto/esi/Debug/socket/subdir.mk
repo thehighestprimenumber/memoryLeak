3c6b18e6b8036704e1b1a8751af6fcd6d73a57d3
nahueldl <deleonardisnahuel@gmail.com>
2018-05-01 18:56:23 -0300

    Revert "Modificada funcion start_listening_select & bug fixed"
    This reverts commit ea8522de58ea751ff6a7d83b1a2559bd97a32223.

--- redistinto/esi/Debug/socket/subdir.mk
+++ redistinto/esi/Debug/socket/subdir.mk
@@ -4,27 +4,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-/home/utnso/tp-2018-1c-Memory-leak/redistinto/socket/operacion.c \
-/home/utnso/tp-2018-1c-Memory-leak/redistinto/socket/socket.c 
+/home/utnso/workspace/tp-2018-1c-Memory-leak/redistinto/socket/socket.c 
 
 OBJS += \
-./socket/operacion.o \
 ./socket/socket.o 
 
 C_DEPS += \
-./socket/operacion.d \
 ./socket/socket.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-socket/operacion.o: /home/utnso/tp-2018-1c-Memory-leak/redistinto/socket/operacion.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-socket/socket.o: /home/utnso/tp-2018-1c-Memory-leak/redistinto/socket/socket.c
+socket/socket.o: /home/utnso/workspace/tp-2018-1c-Memory-leak/redistinto/socket/socket.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
 	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
