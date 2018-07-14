721159f7f86c7b19b15b8217129970a3d784692d
nahueldl <deleonardisnahuel@gmail.com>
2018-07-14 14:43:53 -0300

    kaa

---
+++ redistinto/instancia/Debug/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../bsu.c \
+../circular.c \
+../compactador.c \
+../configuracionInstancia.c \
+../handlerCoordinador.c \
+../instancia.c \
+../lru.c 
+
+OBJS += \
+./bsu.o \
+./circular.o \
+./compactador.o \
+./configuracionInstancia.o \
+./handlerCoordinador.o \
+./instancia.o \
+./lru.o 
+
+C_DEPS += \
+./bsu.d \
+./circular.d \
+./compactador.d \
+./configuracionInstancia.d \
+./handlerCoordinador.d \
+./instancia.d \
+./lru.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
